ef6d666bde773fe5a9aa9f414528b4be05825ece
Kelsey Huse <kelseyhuse30@gmail.com>
2022-03-30 20:40:49 -0500

    updates from testing

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.newPetitionSignature =
+    functions.https.onRequest((request, response) => {
+      const body = request.body;
+      const signature = body[0]["osdi:signature"];
+      const person = signature["person"];
+      const familyName = person["family_name"];
+      const givenName = person["given_name"];
+      functions.logger.info(`Name is: ${givenName} ${familyName}`);
+      functions.logger.info(`Person is: ${JSON.stringify(person)}`);
+      const emailAddress = person["addresses"][0]["address"]; // doesn't work
+      const phoneNumber = person["phone_numbers"][0]["number"]; // doesn't work
+      functions.logger.info(`phone: ${phoneNumber}, email: ${emailAddress}`);
+      response.sendStatus(200);
+    });
